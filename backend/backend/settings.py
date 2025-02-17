accf3143fd64c471b21badae6201490be31860f2
pulkit kapoor <pkapoor@rippling.com>
2025-02-17 13:02:18 +0530

    Update to remove secret and use any value

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qa-m90wuhu(wsagij(#+8dl$v9g3*-e2_*rbjal57t1!7pdjej"
+SECRET_KEY = "setup key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
