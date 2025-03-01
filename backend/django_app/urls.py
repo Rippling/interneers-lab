b8f15b7b0c6f02b712f2d413aab81964a2248e53
VedanshiAwasthi <awasthivedanshi5@gmail.com>
2025-03-01 09:59:41 +0530

    Updated URL patterns, tested API on Postman, resolved Docker engine error, adjusted Docker Compose setup, and verified API response in the browser.

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.http import JsonResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+# def hello_world(request):
+#     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('hello/', hello_world),
+# ]
+
+
+def hello(request):
+    return HttpResponse("Hello, world! This is our interneers-lab Django server, First change made, function name changed from i.e hello_world to hello")
+
+def hello_name(request):
+    """
+    A simple view that returns 'Hello, {name}' in JSON format.
+    Uses a query parameter named 'name'.
+    """
+    # Get 'name' from the query string, default to 'World' if missing
+    name = request.GET.get("name", "World")
+    return JsonResponse({"message": f"Hello, {name}!"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/', hello),
+    path('hello_name/', hello_name),    #test this using -> http://127.0.0.1:8001/hello_name/?name=Vedanshi
 ]
