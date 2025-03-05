75e4833bde28525e2bebf612cc9debe38c4b88a6
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-03-05 23:06:52 +0530

    (feat): Add extra information in responses

--- backend/django_app/controllers/productController.py
+++ backend/django_app/controllers/productController.py
@@ -9,7 +9,7 @@ def productEndpoint(request: HttpRequest, request_id: int= 0):
     if request.method== "POST":
         return addProduct(request)
     if request.method== "PATCH":
-        return updateProduct(request)
+        return updateProduct(request, request_id)
     else:
         error_response= JsonResponse({"message": f"No endpoint for {request.method} request"})
         error_response.status_code= 400
@@ -19,20 +19,26 @@ def addProduct(request: HttpRequest):
     data= json.loads(request.body)
     data["id"]= len(products)+1 
     products.append(data)
-    response= {
-        "id": data["id"],
-    }
-    return JsonResponse(response)
+
+    response= JsonResponse(data)
+    response.status_code= 201
+    response.headers["Location"]= f"/products/{data["id"]}"  # Location of resource
+    return response
 
 def getProduct(request: HttpRequest, request_id: int):
-    if(request_id!=0): 
+    if(request_id!=0): # Reserve 0 id for collection requests
         return JsonResponse(products[request_id-1])
     return JsonResponse(products, safe= False)
 
-def updateProduct(request: HttpRequest):
+def updateProduct(request: HttpRequest, request_id: int):
     data= json.loads(request.body) 
-    request_id= data["id"]
     request_product= products[request_id]
+
+    # Modify each key specified in the request 
     for key in data.keys():
         request_product[key]= data[key]
-    return JsonResponse(products[request_id])
+    
+    response= JsonResponse({})
+    response.headers["Location"]= f"/products/{request_id}"  # Location of resource
+    response.status_code= 204  # No content in body
+    return response
