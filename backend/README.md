153df29fbda80fe67fabe620ae2b771adcd497b0
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-03-06 16:17:57 +0530

    (feat) Add deletion feature, with changes in searching for ID

--- backend/README.md
+++ backend/README.md
@@ -1,8 +1,40 @@
+# Table of Contents:
++ [API Documentation](#api-documentation)
+    + [Products Collection](#products-collection-products)
+        1. [Product Model](#product-model)
+        2. [List All Products](#list-all-products-get)
+        3. [Create a Product](#create-a-new-product-post)
+    + [Product Document](#product-document-productsid)
+        1. [List a Product](#list-a-product-get)
+        2. [Update a Product](#update-a-product-patch)
+        3. [Delete a Product](#delete-a-product-delete)
+    + [Unspecified Endpoints](#unspecified-endpoints)
+
+
 # API Documentation:
 
 ## Products Collection [/products]
 
+### Product model:
+The product model is the primary resource managed by this API. It described a product, in JSON format.
++ Model (application/json)
+
+        {
+            "id": <integer> (required) Auto-assigned ID whenever a product is added,
+            "name": <string> (required) The name of the product,
+            "price": <integer> (required) The price of the product, non-negative values only,
+            "brand": <string> The brand associated with the product, useful for filtering,
+            "category": <string> The category of this item, can only be one of {"Appliances", "Gadgets", "Others"},
+            "quantity": <integer> (required) Quantity of this item in stock, non-negative values only,
+            "description": <string> Description of this item, in English,
+        }
+
+
 ### List All products [GET]
+
+Returns a list of all products currently in the system. The list is guarenteed to be sorted by ID.
+
+Successful request:
 
 + Response 200 (application/json)
 
@@ -26,6 +58,7 @@
                 "id": 2
             }
         ]
+
 
 ### Create a New Product [POST]
 
@@ -58,7 +91,131 @@
                 "id": 2
             }
 
-## Product Document [/products/<id>]
+<details>
+<summary> Common errors </summary>
+<br>
+Invalid name:
+
++ Request (application/json)
+
+        {
+            "name": "",
+            "price": 108,
+            "quantity": 205
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'name' field is invalid",
+            "timestamp": "2025-03-05 18:54:48.326889 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate name field, it must be a non-empty string"
+        }
+
+Missing name:
+
++ Request (application/json)
+
+        {
+            "price": 108,
+            "quantity": 205
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'name' field is required for the product",
+            "timestamp": "2025-03-05 18:59:06.884441 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate name field"
+        }
+
+Invalid price:
+
++ Request (application/json)
+
+        {
+            "name": "Orient Electric 9W High Glow LED bulb| Pack of 2",
+            "price": -1,
+            "quantity": 205
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'price' field is invalid",
+            "timestamp": "2025-03-05 19:25:43.872771 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate price field, it must be a non-negative integer.             Check if you are sending a string instead"
+        }
+
+Missing price:
+
++ Request (application/json)
+
+        {
+            "name": "Orient Electric 9W High Glow LED bulb| Pack of 2",
+            "price": -1,
+            "quantity": 205
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'price' field is required for the product",
+            "timestamp": "2025-03-05 19:27:38.548031 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate price field (non-negative integer)"
+        }
+
+Invalid price:
+
++ Request (application/json)
+
+        {
+            "name": "Orient Electric 9W High Glow LED bulb| Pack of 2",
+            "price": 108,
+            "quantity": -1
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'quantity' field is invalid",
+            "timestamp": "2025-03-05 19:25:43.872771 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate quantity field, it must be a non-negative integer.             Check if you are sending a string instead"
+        }
+
+Missing quantity:
+
++ Request (application/json)
+
+        {
+            "name": "Orient Electric 9W High Glow LED bulb| Pack of 2",
+            "price": 108,
+        }
++ Response 400 (application/json)
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "'quantity' field is required for the product",
+            "timestamp": "2025-03-05 19:27:38.548031 GMT+0:00",
+            "request": "POST /products",
+            "suggestion": "Re-send the request with an appropriate quantity field (non-negative integer)"
+        }
+
+</details>        
+
+
+## Product Document [/products/\<id\>]
 
 ### List a Product [GET]
 
@@ -85,3 +242,24 @@
 + Response 204 (application/json)  
 
         No content
+
+### Delete a Product [DELETE]
+
++ Response 204 (application/json)
+
+        No content
+
+
+## Unspecified Endpoints:
+
+Trying to access any unspecified endpoint, ie, an unspecified method on a URI that exists, returns
+a typical error message.
+
+        {
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "No endpoint for PUT request",
+            "timestamp": "2025-03-05 18:18:32.320301 GMT+0:00",
+            "request": "PUT /products",
+            "suggestion": "Check the documentation at https://github.com/Alph3ga/interneers-lab for the available API endpoints"
+        }
--- backend/django_app/controllers/productController.py
+++ backend/django_app/controllers/productController.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
 import json
+from datetime import datetime
+import math
 
 products= []
 
@@ -10,13 +12,36 @@ def productEndpoint(request: HttpRequest, request_id: int= 0):
         return addProduct(request)
     if request.method== "PATCH":
         return updateProduct(request, request_id)
+    if request.method== "DELETE":
+        return deleteProduct(request, request_id)
     else:
-        error_response= JsonResponse({"message": f"No endpoint for {request.method} request"})
+        error_response= JsonResponse({
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": f"No endpoint for {request.method} request",
+            "timestamp": f"{datetime.now()} GMT+0:00",
+            "request": f"{request.method} {request.path}",
+            "suggestion": "Check the documentation at https://github.com/Alph3ga/interneers-lab for the available API endpoints"
+            })
         error_response.status_code= 400
         return error_response
 
 def addProduct(request: HttpRequest):
     data= json.loads(request.body)
+    validation= validate_product(data)
+
+    if(not validation["valid"]):
+        error_response= JsonResponse({
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": validation["details"],
+            "timestamp": f"{datetime.now()} GMT+0:00",
+            "request": f"{request.method} {request.path}",
+            "suggestion": validation["suggestion"],
+            })
+        error_response.status_code= 400
+        return error_response
+
     data["id"]= len(products)+1 
     products.append(data)
 
@@ -27,18 +52,139 @@ def addProduct(request: HttpRequest):
 
 def getProduct(request: HttpRequest, request_id: int):
     if(request_id!=0): # Reserve 0 id for collection requests
-        return JsonResponse(products[request_id-1])
-    return JsonResponse(products, safe= False)
+        index= findProduct(request_id)
+
+        if(index== -1):
+            error_response= JsonResponse({
+                "code": "NOT_FOUND",
+                "message": "The requested resource was not found",
+                "details": f"Product with id {request_id} does not exist",
+                "timestamp": f"{datetime.now()} GMT+0:00",
+                "request": f"{request.method} {request.path}",
+                "suggestion": "Use 'GET /products' to get a list of existing products with id",
+                })
+            error_response.status_code= 404
+            return error_response
+
+        return JsonResponse(products[index])
+    
+        return JsonResponse(products, safe= False)
+
 
 def updateProduct(request: HttpRequest, request_id: int):
     data= json.loads(request.body) 
-    request_product= products[request_id]
+
+    index= findProduct(request_id)
+    if(index== -1):
+        error_response= JsonResponse({
+            "code": "NOT_FOUND",
+            "message": "The requested resource was not found",
+            "details": f"Product with id {request_id} does not exist",
+            "timestamp": f"{datetime.now()} GMT+0:00",
+            "request": f"{request.method} {request.path}",
+            "suggestion": "Use 'GET /products' to get a list of existing products with id",
+            })
+        error_response.status_code= 404
+        return error_response
+
+    request_product= products[index]
 
     # Modify each key specified in the request 
     for key in data.keys():
+        if(key=="id"):
+            error_response= JsonResponse({
+            "code": "BAD_REQUEST",
+            "message": "The server cannot process this request",
+            "details": "Product ID cannot be updated",
+            "timestamp": f"{datetime.now()} GMT+0:00",
+            "request": f"{request.method} {request.path}",
+            "suggestion": "Remove 'id' field from your request, or check if it matches the URI",
+            })
+        error_response.status_code= 404
+        return error_response
         request_product[key]= data[key]
     
     response= JsonResponse({})
     response.headers["Location"]= f"/products/{request_id}"  # Location of resource
     response.status_code= 204  # No content in body
     return response
+
+def deleteProduct(request: HttpRequest, request_id: int):
+    index= findProduct(request_id)
+    if(index== -1):
+        error_response= JsonResponse({
+            "code": "NOT_FOUND",
+            "message": "The requested resource was not found",
+            "details": f"Product with id {request_id} does not exist",
+            "timestamp": f"{datetime.now()} GMT+0:00",
+            "request": f"{request.method} {request.path}",
+            "suggestion": "Use 'GET /products' to get a list of existing products with id",
+            })
+        error_response.status_code= 404
+        return error_response
+    products.pop(index)
+    
+    response= JsonResponse({})
+    response.status_code= 204
+    return response
+
+def validate_product(data):
+    if "name" not in data:
+        return {
+            "valid": False,
+            "details": "'name' field is required for the product",
+            "suggestion": "Re-send the request with an appropriate name field",
+        }
+    if type(data["name"]) is not str or len(data["name"])== 0:
+        return {
+            "valid": False,
+            "details": "'name' field is invalid",
+            "suggestion": "Re-send the request with an appropriate name field, it must be a non-empty string",
+        }
+
+
+    if "price" not in data:
+        return {
+            "valid": False,
+            "details": "'price' field is required for the product",
+            "suggestion": "Re-send the request with an appropriate price field (non-negative integer)",
+        }
+    if type(data["price"]) is not int or data["price"]< 0:
+        return {
+            "valid": False,
+            "details": "'price' field is invalid",
+            "suggestion": "Re-send the request with an appropriate price field, it must be a non-negative integer. \
+            Check if you are sending a string instead",
+        }
+
+    if "quantity" not in data:
+        return {
+            "valid": False,
+            "details": "'quantity' field is required for the product",
+            "suggestion": "Re-send the request with an appropriate quantity field (non-negative integer)",
+        }
+    if type(data["quantity"]) is not int or data["quantity"]< 0:
+        return {
+            "valid": False,
+            "details": "'quantity' field is invalid",
+            "suggestion": "Re-send the request with an appropriate quantity field, it must be a non-negative integer. \
+            Check if you are sending a string instead",
+        }
+    
+    return { "valid": True}
+
+def findProduct(request_id: int):
+    # binary search through the products array
+
+    l= 0
+    r= len(products)
+    
+    while(l<r):
+        mid= (l+r)//2
+        if(products[mid]["id"]== request_id):
+            return mid
+        if(products[mid]["id"]< request_id):
+            l= mid+1
+        else:
+            r= mid
+    return -1
