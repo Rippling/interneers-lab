bd4962ded1e842b655e73ade2156471226031b78
Gaurav-160 <103301239+Gaurav-160@users.noreply.github.com>
2025-03-06 09:27:13 +0530

    implemented the hello_name endpoint

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server.")
+    return HttpResponse("Hello, world! This is our interneers-lab Django server. And this app is running in my local machine..... Let's get started with the project.")
+
+def hello_name(request):
+    name = request.GET.get("name", "World") # default will be World if value for name is not found
+    data = {
+        "message": f"Hello {name} !" 
+    }
+    return JsonResponse(data)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+    path('hello_name/', hello_name)
 ]
