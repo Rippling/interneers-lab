02c41b709faecff3df846cb9b8be5cf8b0154362
GargiPandey27 <togargi27pandey@gmail.com>
2025-03-09 19:33:13 +0530

    Modified urls.py

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import create_product, get_all_products, get_product, update_product, delete_product
 
 urlpatterns = [
-    path("", get_all_products, name="api_home"),  # Default when visiting /api/
+    path("", get_all_products, name="api_home"),  
     path("create/", create_product, name="create_product"),
     path("get_all/", get_all_products, name="get_all_products"),
     path("get/<int:product_id>/", get_product, name="get_product"),
