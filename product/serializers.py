07c55722f4733981fb8feeca59f74f7e607b2921
GunjanTewari1404 <gunjantewari1404@gmail.com>
2025-03-16 01:06:33 +0530

    added serializers for Product and ProductHistory

--- product/serializers.py
+++ product/serializers.py
@@ -1,25 +1,41 @@
 from rest_framework import serializers
-from .models import Product  # Import the Product model
+from .models import Product
+from datetime import datetime,timezone      #for handling timestamps
 
-class ProductSerializer(serializers.ModelSerializer):  # Use ModelSerializer
-    class Meta:
-        model = Product
-        fields = '__all__'  # Automatically includes all model fields
+#serializer for prod. model
+class ProductSerializer(serializers.Serializer):
+    id=serializers.CharField(read_only=True)
+    name=serializers.CharField(required=True,max_length=200)
+    description=serializers.CharField(required=False)
+    price=serializers.DecimalField(max_digits=10, decimal_places=2)
+    category=serializers.CharField(required=False)
+    brand=serializers.CharField(required=True)
+    quantity=serializers.IntegerField(required=True)
+    created_at=serializers.DateTimeField(read_only=True)
+    updated_at=serializers.DateTimeField(read_only=True)
 
-    def validate_name(self, value):
-        """Ensure the product name is unique."""
-        if Product.objects.filter(name=value).exists():
-            raise serializers.ValidationError("A product with this name already exists.")
-        return value
+#serializer for prod. history track
+class ProductHistorySerializer(serializers.Serializer):
+    product_id=serializers.CharField()
+    name=serializers.CharField()
+    description=serializers.CharField()
+    category=serializers.CharField()
+    price=serializers.DecimalField(max_digits=10, decimal_places=2)
+    brand=serializers.CharField()
+    quantity=serializers.IntegerField()
+    version_created_at=serializers.DateTimeField()    
 
-    def validate_price(self, value):
-        """Ensure the price is non-negative."""
-        if value < 0:
-            raise serializers.ValidationError("Price must be a non-negative value.")
-        return value
+#method to create a new prod. history
+    def create(self, valid_data):
+        now=datetime.now(timezone.utc) 
+        valid_data['created_at']=now
+        valid_data['updated_at']=now
+        return Product(**valid_data).save()
 
-    def validate_quantity(self, value):
-        """Ensure the quantity is non-negative."""
-        if value < 0:
-            raise serializers.ValidationError("Quantity must be a non-negative value.")
-        return value
+#method to update existing prod. history
+    def update(self, instance, valid_data):
+        for attr, value in valid_data.items():
+            setattr(instance, attr, value)
+        instance.updated_at=datetime.now(timezone.utc) 
+        instance.save()
+        return instance
