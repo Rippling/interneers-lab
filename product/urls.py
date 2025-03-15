3708ef2ba4da053db33792d6b97aa0172b78fdcb
GunjanTewari1404 <gunjantewari1404@gmail.com>
2025-03-16 01:07:56 +0530

    added url paths for managing and tracking products

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import ProductAPI
+from .views import ProductListView,ProductDetailView,RecentUpdatedProductsView,ProductHistoryView
+# from .views import ProductAPI
+
+# urlpatterns = [
+#     path('products/', ProductAPI.as_view(), name='product-list'),
+#     path('products/<int:product_id>/', ProductAPI.as_view(), name='product-detail'),
+# ]
+
 
 urlpatterns = [
-    path('products/', ProductAPI.as_view(), name='product-list'),
-    path('products/<int:product_id>/', ProductAPI.as_view(), name='product-detail'),
+    path('products/',ProductListView.as_view(),name='product-list'),
+    path('products/<str:prod_id>/',ProductDetailView.as_view(),name='product-detail'),
+    path('recent-updates/',RecentUpdatedProductsView.as_view(),name='recent-updates'),
+    path('product-history/<str:prod_id>/',ProductHistoryView.as_view(),name='product-history')
 ]
