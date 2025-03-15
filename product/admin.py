1e301e99359f368c7cf73d16a56c9647e59dedb8
GunjanTewari1404 <gunjantewari1404@gmail.com>
2025-03-16 01:12:03 +0530

    added other required files

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,2 @@
 from django.contrib import admin
-from .models import Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'brand', 'quantity', 'created_at')
-    search_fields = ('name', 'brand', 'category')
-    list_filter = ('category', 'brand')
 
