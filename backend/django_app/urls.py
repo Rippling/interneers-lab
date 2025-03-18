dab5af836f4e19d984cfbcf8c812aae3f11528c1
GargiPandey27 <togargi27pandey@gmail.com>
2025-03-18 21:33:34 +0530

    Updated URL patterns to include product-related endpoints

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,12 +1,7 @@
-# django_app/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from product.views import get_all_products
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", get_all_products, name="home"),
-    path("product/", include("product.urls")),
-
+    path("admin/", admin.site.urls),
+    path("products/", include("product.urls")),  
 ]
