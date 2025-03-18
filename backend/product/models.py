c96806d2801f7fe163cbf25c9c7ee7b4b89074bd
GargiPandey27 <togargi27pandey@gmail.com>
2025-03-18 21:30:18 +0530

    Added Product model for MongoDB schema

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,15 +1,17 @@
-from django.db import models
+from mongoengine import Document, StringField, FloatField, IntField, DateField
 
-class Product(models.Model):
-    name = models.CharField(max_length=300)
-    category = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    brand = models.CharField(max_length=100)
-    quantity = models.IntegerField()
-    manufacture_date = models.DateField()
-    expiry_date = models.DateField()  # for food, medicine, and items with a shelf life
-    weight = models.FloatField() 
+class Product(Document):
+    name = StringField(required=True, unique=True)
+    description = StringField()
+    category = StringField(required=True)
+    price_in_RS = FloatField(required=True, default=0.0)
+    brand = StringField()
+    quantity = IntField(required=True)
+    manufacture_date = DateField(required=True)
+    expiry_date = DateField(required=True)
+    weight_in_KG = FloatField(default=0.0)
 
-    def __str__(self):
-        return self.name
+    meta = {
+        "collection": "products",
+        "indexes": ["name", "category", "price_in_RS", "brand"]
+    }
