03af886904a56a12333e208d5fb8552455b5b7af
GargiPandey27 <togargi27pandey@gmail.com>
2025-03-18 21:32:34 +0530

    Added product API routes

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from .views import create_product, get_all_products, get_product, update_product, delete_product
+from .views import ProductController
 
 urlpatterns = [
-    path("", get_all_products, name="api_home"),  
-    path("create/", create_product, name="create_product"),
-    path("get_all/", get_all_products, name="get_all_products"),
-    path("get/<int:product_id>/", get_product, name="get_product"),
-    path("update/<int:product_id>/", update_product, name="update_product"),
-    path("delete/<int:product_id>/", delete_product, name="delete_product"),
-    
+    path("", ProductController.as_view()),  
+    path("<str:product_id>/", ProductController.as_view()),  
 ]
