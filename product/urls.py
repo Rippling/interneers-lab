83733e071eae691c53cb8f154e8d7c21a9640a7a
GunjanTewari1404 <gunjantewari1404@gmail.com>
2025-03-30 11:35:24 +0530

    updated to include url paths for categories and their management

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import ProductListView,ProductDetailView,RecentUpdatedProductsView,ProductHistoryView
+from .views.ProductView import ProductListView,ProductDetailView,RecentUpdatedProductsView,ProductHistoryView
+from .views.CategoryView import (
+    ProdCategoryListView, 
+    ProdCategoryDetailView, 
+    CategoryProductsView,
+    ProdCategoryManagementView
+)
+
 # from .views import ProductAPI
 
 # urlpatterns = [
@@ -12,5 +19,10 @@ urlpatterns = [
     path('products/',ProductListView.as_view(),name='product-list'),
     path('products/<str:prod_id>/',ProductDetailView.as_view(),name='product-detail'),
     path('recent-updates/',RecentUpdatedProductsView.as_view(),name='recent-updates'),
-    path('product-history/<str:prod_id>/',ProductHistoryView.as_view(),name='product-history')
+    path('product-history/<str:prod_id>/',ProductHistoryView.as_view(),name='product-history'),
+    path('categories/',ProdCategoryListView.as_view(),name='category-list'),
+    path('categories/id/<str:category_id>/',ProdCategoryDetailView.as_view(),name='category-detail'),
+    path('categories/title/<str:title>/',ProdCategoryDetailView.as_view(),name='category-search'),
+    path('categories/<str:category_id>/products/',CategoryProductsView.as_view(),name='category-products'),
+    path('prod-categories/manage-products/',ProdCategoryManagementView.as_view(),name='category-product-management'),   
 ]
