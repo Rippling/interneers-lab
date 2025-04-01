b78150686caf3426db6c19613b394e694d77104b
GargiPandey27 <togargi27pandey@gmail.com>
2025-04-01 17:08:26 +0530

    Added categories api_endpoint urls

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductController
+from product.views.product_views import ProductController
+from product.views.product_category_views import ProductCategoryView, SingleProductCategoryView
 
 urlpatterns = [
+    path("categories/", ProductCategoryView.as_view(), name="category_list"),
+    path("categories/<str:category_id>/", SingleProductCategoryView.as_view(), name="category_detail"),
     path("", ProductController.as_view()),  
     path("<str:product_id>/", ProductController.as_view()),  
 ]
