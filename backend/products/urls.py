8d412059e82e95ded3f0535a821d408b5f513bb9
Ashok Chaudhari <92539504+AshokChaudhari07@users.noreply.github.com>
2025-04-04 04:49:01 +0530

    feat: Add repository, service, and controller layers for ProductCategory with CRUD operations

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from products.controllers.product_controller import productsView, productDetailView
+from products.controllers.product_category_controller import productCategoryView,productCategoryDetailView\
 
 urlpatterns = [
     path("products/", productsView),
     path("products/<str:id>/", productDetailView),  # id changed to str
+
+    path("categories/", productCategoryView),
+    path("categories/<str:id>/", productCategoryDetailView),  # id changed to str
 ]
