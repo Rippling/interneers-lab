7880e147232f08dd2fcfde689a174686252b0169
VedanshiAwasthi <awasthivedanshi5@gmail.com>
2025-04-06 16:57:19 +0530

    updated urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -12,7 +12,7 @@ from .views.ProductView import (
     
 )
 
-from .views.CategoryView import (CategoryView , CategoryDetail)
+from .views.CategoryView import (CategoryView , CategoryDetail, CategoryList)
 
 urlpatterns = [
     path('products/create/', ProductCreate.as_view(), name='create-product'),
@@ -21,7 +21,9 @@ urlpatterns = [
     path('products/<str:id>/update/', ProductUpdate.as_view(), name='product-update'), 
     path('products/<str:id>/delete/', ProductDelete.as_view(), name='product-delete'),  
     # path("check-category/", CheckCategoryView.as_view(), name="check_category"),
-    path('categories/', CategoryView.as_view(), name='category-list'),
+    path('categories/', CategoryView.as_view(), name='category-create'),
+    path('categories/all', CategoryList.as_view(), name='category-list'),
+    path('categories/<str:title>/update', CategoryView.as_view(), name='category-update'),      
     path('categories/title/<str:title>/', CategoryView.as_view(), name='category-title-detail'),
     path('categories/id/<str:id>/', CategoryDetail.as_view(), name='category-id-detail'),      
     path('products/<str:product_id>/add-category/<str:category_id>/', AddCategoryToProduct.as_view(), name='add_category_to_product'),
