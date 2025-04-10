d4305b59ca1f0f8d875aa6be698eaee17786fccc
GargiPandey27 <togargi27pandey@gmail.com>
2025-04-11 02:06:40 +0530

    Refactor: shifted business logic to service layer

--- backend/product/repositories/product_category_repository.py
+++ backend/product/repositories/product_category_repository.py
@@ -1,12 +1,16 @@
 from product.models.category import ProductCategory
 from product.models.product_model import Product
+from mongoengine.errors import NotUniqueError
 from bson import ObjectId
 
 class ProductCategoryRepository:
     def create_category(self, category_data):
-        category = ProductCategory(**category_data)
-        category.save()
-        return category
+        try: 
+            category = ProductCategory(**category_data)
+            category.save()
+            return category
+        except NotUniqueError:
+            raise
 
     def get_all_categories(self):
         return ProductCategory.objects.filter(is_active=True)
@@ -16,23 +20,13 @@ class ProductCategoryRepository:
             return None
         return ProductCategory.objects(id=ObjectId(category_id)).first()
 
-    def update_category(self, category_id, updated_data):
-        category = self.get_category_by_id(category_id)
-        if category:
-            for key, value in updated_data.items():
-                setattr(category, key, value)
-            category.save()
-            return category
-        return None
+    def update_category(self, category, updated_data):
+        for key, value in updated_data.items():
+            setattr(category, key, value)
+        category.save()
+        return category
 
-    def delete_category(self, category_id):
-        category = ProductCategory.objects(id=category_id).first()
-        if not category:
-            return False
-
-        # Removes this category from all products
+    def delete_category(self, category):
         Product.objects(category=category).update(pull__category=category)
-
-        # Then deletes the category
         category.delete()
         return True
--- backend/product/services/product_category_service.py
+++ backend/product/services/product_category_service.py
@@ -1,21 +1,33 @@
-from product.models.category import ProductCategory
+from mongoengine.errors import NotUniqueError
+from bson import ObjectId
 from product.repositories.product_category_repository import ProductCategoryRepository
 
 class ProductCategoryService:
     def __init__(self):
-        self.product_category_repository = ProductCategoryRepository()
-        
+        self.repo = ProductCategoryRepository()
+
     def create_category(self, category_data):
-        return self.product_category_repository.create_category(category_data)
+        try:
+            return self.repo.create_category(category_data)
+        except NotUniqueError:
+            raise ValueError("Category with this name already exists.")
 
     def get_all_categories(self):
-        return self.product_category_repository.get_all_categories()
+        return self.repo.get_all_categories()
 
     def get_category_by_id(self, category_id):
-        return self.product_category_repository.get_category_by_id(category_id)
+        if not ObjectId.is_valid(category_id):
+            return None
+        return self.repo.get_category_by_id(category_id)
 
     def update_category(self, category_id, updated_data):
-        return self.product_category_repository.update_category(category_id, updated_data)
+        category = self.get_category_by_id(category_id)
+        if not category:
+            return None
+        return self.repo.update_category(category, updated_data)
 
     def delete_category(self, category_id):
-        return self.product_category_repository.delete_category(category_id)
+        category = self.get_category_by_id(category_id)
+        if not category:
+            return False
+        return self.repo.delete_category(category)
