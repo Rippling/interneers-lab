ca527efd39066076c8338ff2af790b487bd3c49a
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-04-10 12:26:25 +0530

    feat(backend): Add category endpoint URI to router

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 from src.controllers.product_controller import product_endpoint
+from src.controllers.product_category_controller import category_product_endpoint
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
@@ -12,4 +13,5 @@ urlpatterns = [
     path('hello/', hello_world),
     path('products', product_endpoint),
     path('products/<slug:request_id>', product_endpoint),
+    path('categories/<slug:category_title>', category_product_endpoint),
 ]
