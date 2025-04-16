8e3e9a146faa295c4bae3e2a822d482e48c58182
VedanshiAwasthi <awasthivedanshi5@gmail.com>
2025-04-16 19:55:32 +0530

    Add static product list with name, price, description, and image

---
+++ frontend/src/data/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 70000,
+    description: "A powerful laptop for developers.",
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.Nb7ovZ6o8bs5nekRo2pU3gHaFo&pid=Api&P=0&h=180",
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    price: 30000,
+    description: "Latest model with AI camera.",
+    image: "https://cdn.mos.cms.futurecdn.net/u6ACPLnZhHS9fmJGE772nU.jpg",
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: 5000,
+    description: "Noise cancelling headphones.",
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.GBiqLlgWUpPUulP1YyaovgHaE7&pid=Api&P=0&h=180",
+  },
+];
