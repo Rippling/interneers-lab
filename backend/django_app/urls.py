3cd2dc922db41e63ee6945e3a8a9e2e7bd9fb08b
VedanshiAwasthi <awasthivedanshi5@gmail.com>
2025-04-16 14:27:35 +0530

    feat: add new URL paths for product and category pages

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,39 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.conf.urls.static import static
+from django.conf import settings  # Import settings here
 
-# def hello_world(request):
-#     return HttpResponse("Hello, world! This is our interneers-lab Django server.")
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('hello/', hello_world),
-# ]
-
-
-def home(request):
-    return HttpResponse("Hi")
-
-
-def hello(request):
-    return HttpResponse("Hello, world! This is our interneers-lab Django server, First change made, function name changed from i.e hello_world to hello")
-
-def hello_name(request):
-    """
-    A simple view that returns 'Hello, {name}' in JSON format.
-    Uses a query parameter named 'name'.
-    """
-    # Get 'name' from the query string, default to 'World' if missing
-    name = request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hello, {name}!"})
-
+from products.views1 import show_product_page , product_detail_view , show_categories_page, category_detail_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello),
-    path('', home),
-    path('hello_name/', hello_name),    #test this using -> hello_name/?name=Vedanshi
-    #tested with few more apis like http://127.0.0.1:8000/hello_name/?name=John%20Doe!@#   , etc
-   path('api/', include('products.urls')), 
-]
+    path('api/', include('products.urls')), 
+    path('products/page/', show_product_page, name='product_page'),
+    path('products/<str:id>/', product_detail_view, name='product-detail'),
+    path('categories/page/', show_categories_page, name='category_page'),
+    path('categories/title/<str:title>', category_detail_view, name='category-detail-products'),
+] 
