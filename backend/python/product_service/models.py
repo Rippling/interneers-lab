a7e49190552b38ec4c29ab77235ee1ce150a8db9
Pushkar06p <pushkar06pandey@gmail.com>
2026-03-14 09:51:30 +0530

    Update product model

--- backend/python/product_service/models.py
+++ backend/python/product_service/models.py
@@ -1,7 +1,11 @@
-from mongoengine import Document, StringField, FloatField
+from mongoengine import Document, StringField, FloatField, IntField
 
 class Product(Document):
     name=StringField(required=True)
     description=StringField()
+    category = StringField(required=True)
     price=FloatField(required=True)
+    brand=StringField()
+    quantity=IntField(required=True)
+
     
