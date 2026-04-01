2f5ecb35ed7890889216e379672d885e243f45a7
Pushkar06p <pushkar06pandey@gmail.com>
2026-04-01 13:28:26 +0530

    feat: update product and category models and repositories

--- backend/python/product_service/models.py
+++ backend/python/product_service/models.py
@@ -1,18 +1,35 @@
-from mongoengine import Document, ReferenceField, StringField, FloatField, IntField
-from product_category.models import ProductCategory
+import datetime
+from mongoengine import Document, ReferenceField, StringField, FloatField, IntField, DateTimeField, NULLIFY
 
 
+class Category(Document):
+    name=StringField(required=True)
+    description=StringField()     
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+    
+    def save(self, *args, **kwargs):
 
+        self.updated_at = datetime.datetime.now()
+        return super().save(*args, **kwargs)      
+
+    
+
+    
 class Product(Document):
     name=StringField(required=True)
     description=StringField()
-    category = ReferenceField(ProductCategory)
+    category = ReferenceField(Category,reverse_delete_rule=NULLIFY) # reverse_delete_rule=NULLIFY
     price=FloatField(required=True)
     brand=StringField(required=True)
     quantity=IntField(required=True)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
 
-    def clean(self):
-        if not self.brand or self.brand.strip() == "":
-            raise ValueError("Brand cannot be empty or whitespace")
+    def save(self, *args, **kwargs):
 
+        self.updated_at = datetime.datetime.now()
+        return super().save(*args, **kwargs)
     
+    
+
--- backend/python/product_service/repository.py
+++ backend/python/product_service/repository.py
@@ -1,5 +1,6 @@
-from .models import Product
-from product_category.models import ProductCategory
+from bson import ObjectId
+from .models import Product, Category
+
 
 class ProductRepository:
 
@@ -8,36 +9,64 @@ class ProductRepository:
         product = Product(**data)
         product.save()
         return product
-    
+
     @staticmethod
-    def get_all():
-        return Product.objects()
-    
+    def get_all(sort_by):
+        allowed_sorts = ['name', '-name', 'price', '-price', 'created_at', '-created_at', 'updated_at', '-updated_at']
+        
+        if sort_by not in allowed_sorts:
+            sort_by = '-updated_at'
+        return Product.objects().order_by(sort_by)
+
     @staticmethod
-    def get_all_by_category_id(category_id):
-        return Product.objects(category=category_id)
-    
+    def get_by_id(id):
+        return Product.objects(id=id).first()
+
     @staticmethod
-    def remove_category_from_product(product_id):
-        product = Product.objects(id=product_id).first()
-        if product:
-            product.category = None
-            product.save()
-        return product    
-    
-    @staticmethod
-    def add_category_to_product(product_id, category_id):
-        product = Product.objects(id=product_id).first()
-        category = ProductCategory.objects(id=category_id).first()
-        if product:
-            product.category = category
-            product.save()
+    def update(product):
+        product.save()
         return product
 
     @staticmethod
-    def get_by_id(product_id):
-        return Product.objects(id=product_id).first()
-    
+    def delete(id):
+        product = Product.objects(id=id).first()
+        if product:
+            product.delete()
+        return product
+
     @staticmethod
-    def delete_by_id(product_id):
-        Product.objects(id=product_id).delete()
+    def get_all_by_category_id(category_id, sort_by):
+        allowed_sorts = ['name', '-name', 'price', '-price', 'created_at', '-created_at', 'updated_at', '-updated_at']
+        
+        if sort_by not in allowed_sorts:
+            sort_by = '-updated_at'
+        return Product.objects(category=category_id).order_by(sort_by)
+
+
+class CategoryRepository:
+
+    @staticmethod
+    def create(data):
+        category = Category(**data)
+        category.save()
+        return category
+
+    @staticmethod
+    def get_all():
+        return Category.objects()
+
+    @staticmethod
+    def get_by_id(category_id,):
+        return Category.objects(id=category_id).first()
+
+    @staticmethod
+    def update(category):
+        category.save()
+        return category
+
+    @staticmethod
+    def delete(category_id):
+        category = Category.objects(id=category_id).first()
+        if category:
+            category.delete()
+        return category
